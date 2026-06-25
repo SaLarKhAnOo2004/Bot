d8222e45ae6803671836eb606efdcde8383cdcfb
SaLarKhAnOo2004 <salarkhanoo2009@gmail.com>
2026-06-25 14:02:04 +0430

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+# config.py
+
+# د Telegram بوټ توکنونه
+USER_BOT_TOKEN = "8540384399:AAEFZh1kne1KgXgDkXvzaesUg6GBOSNO0Fg"  # د کارونکي بوټ
+AUTO_BOT_TOKEN = "8890120628:AAFf-cLeuNw1PXLKGN9L6nDVR26L-3yNzn0"  # د اتومات بوټ
+
+# د مدیرانو ID لیست (یوازې دا کسان کولای شي بوټ وکاروي)
+ADMIN_IDS = [5887665463]  # خپل Telegram ID دلته دننه کړئ
+
+# د OnlineSim API تنظیمات (که API کیلي لرئ)
+ONLINESIM_API_KEY = ""  # که نه لرئ، خالي پرېږدئ
+ONLINESIM_BASE_URL = "https://onlinesim.io/api"
+
+# د ډیټابیس فایل نوم
+DATABASE_FILE = "numbers.db"
+
+# د اتومات بوټ تنظیمات
+AUTO_CHECK_INTERVAL = 30  # هره ۳۰ ثانیه یو ځل چیک کړي
+MAX_NUMBERS_PER_COUNTRY = 50  # د هر هیواد لپاره ډېرې شمېرې
